9336dbe945d1cb5c817cb2b5c0c31423a287dbb9
mjkmike <mks9377@gmail.com>
2016-10-26 14:50:51 -0400

    remove span

--- www/assets/js/helper.js
+++ www/assets/js/helper.js
@@ -8,11 +8,10 @@ var Helper = {
 			for (var i=0; i<dataArray.length; i++){
 				var _displayItem = itemRenderFunction(dataArray[i]);
 				var _keyField = dataArray[i][keyField];
-
 				if(bottompage)
 					var _html = "<li data-icon='false' keyField='" + _keyField + "'><a class='pageAnchor' disabled='disabled' href='javascript:void(0)'>" + itemRenderFunction(dataArray[i]) + "</a></li>\n";
 				else
-					var _html = "<li keyField='" + _keyField + "'><a class='pageAnchor' disabled='disabled' href='javascript:void(0)'><span>" + itemRenderFunction(dataArray[i]) + "</span></a></li>\n";
+					var _html = "<li keyField='" + _keyField + "'><a class='pageAnchor' disabled='disabled' href='javascript:void(0)'>" + itemRenderFunction(dataArray[i]) + "</a></li>\n";
 				$(listviewComponent).append(_html);
 			}
 
